e08ffc0ea8b8df3942f591ca265757f27c4b4b16
zazapolat58 <60259098+zazapolat58@users.noreply.github.com>
2021-10-22 22:45:13 +0300

    Add files via upload

---
+++ commands/şarkı.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "şarkı",
+  description: "Electus <3 Iceland",
+  execute (client, message, args) {
+    
+      const { channel } = message.member.voice;
+    if (!channel) {
+      return message.channel.send("**Herhangi bir ses kanalında bulunmalısınız.**");
+    }
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+
+    if (!serverQueue) {
+      return message.channel.send("**Herhangi bir şarkı oynatmıyorum.**");
+    }
+    message.channel.send(serverQueue.songs[0].title + ' - **Şuanda Oynatılan Şarkı.**')
+
+    
+    
+    
+  }
+}
